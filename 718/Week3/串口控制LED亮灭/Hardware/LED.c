ef03c983573c4d388dedd6ce18b53ca6e7091235
Ruakker <i@ruakker.moe>
2024-10-20 18:20:31 +0800

    upd: week3 - 串口控制LED亮灭 - 闪烁LED

--- 718/Week3/串口控制LED亮灭/Hardware/LED.c
+++ 718/Week3/串口控制LED亮灭/Hardware/LED.c
@@ -1,20 +1,31 @@
-#include "stm32f10x.h"                  // Device header
+#include "Timer.h"
+#include "stm32f10x.h"  // Device header
 
 void LED1_Init(void) {
+    Timer_Init();
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-    
+
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;  // 开漏输出
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    
+
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
-void LED1_ON(void) {
-    GPIO_ResetBits(GPIOB, GPIO_Pin_0);
-}
+void LED1_ON(void) { TIM_Cmd(TIM2, ENABLE); }
 
 void LED1_OFF(void) {
+    TIM_Cmd(TIM2, DISABLE);
     GPIO_SetBits(GPIOB, GPIO_Pin_0);
+}
+
+void TIM2_IRQHandler(void) {
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET) {
+        if (GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_0))
+            GPIO_ResetBits(GPIOB, GPIO_Pin_0);
+        else
+            GPIO_SetBits(GPIOB, GPIO_Pin_0);
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
 }
---
+++ 718/Week3/串口控制LED亮灭/System/Timer.c
@@ -0,0 +1,29 @@
+#include "stm32f10x.h"  // Device header
+
+// TIM2 100 ms
+
+void Timer_Init(void) {
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    TIM_InternalClockConfig(TIM2);
+
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseStructure.TIM_Period = 1000 - 1;     // ARR = 1000
+    TIM_TimeBaseStructure.TIM_Prescaler = 7200 - 1;  // PSC = 7200
+    TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+    TIM_ClearFlag(TIM2, TIM_FLAG_Update);
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_Init(&NVIC_InitStructure);
+}
